1c9da1feb24ea5716f2f086583ff95d76f3940bd
unknown <alecs2art@gmail.com>
2021-08-09 19:40:57 -0700

    some refactoring

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const express = require('express');
 const session = require('express-session');
-const exphbs = require('express-handlebars');
+
 
 
 const app = express();
@@ -15,24 +15,17 @@ app.use(express.urlencoded({ extended: false}))
 
 
 app.get("./", (req, res) => {
-  res.sendFile(__dirname + "/index.html");
+  res.sendFile(__dirname + "./index.html");
 });
 
-app.get('./login', (req, res) => {
-  res.render('login.ejs')
+app.get('./login-register', (req, res) => {
+  res.render('./login-register.ejs')
 });
 
-app.post('./login', (req, res) => {
-
-})
-
-app.get('./register', (req, res) => {
-  res.render('register.ejs')
-});
-
-app.post('./register', (req, res) => {
+app.post('./login-register', (req, res) => {
 
 });
+
 
 
 const sess = {
@@ -50,9 +43,9 @@ app.use(session(sess));
 const helpers = require('./utils/helpers');
 const { Server } = require('http');
 
-const hbs = exphbs.create({ helpers });
 
-app.engine('handlebars', hbs.engine);
+
+
 
 
 app.use(express.json());
