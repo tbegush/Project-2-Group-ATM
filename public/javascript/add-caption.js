31c6a0c3f8bd3a87151cecaa95b488dd4406ec1d
Thomas Begush <tbegush@gmail.com>
2021-08-11 19:55:49 -0400

    capturing the caption

--- public/javascript/add-caption.js
+++ public/javascript/add-caption.js
@@ -1,26 +1,24 @@
-async function newFormHandler(event) {
-    event.preventDefault();
-  
-    const user_id = 11;
-    const caption_text = document.querySelector('#caption-text-area').value;
-  console.log("we captures the caption", caption_text);
-    const response = await fetch(`/api/captions`, {
-      method: 'POST',
-      body: JSON.stringify({
-        caption_text,
-        user_id
-      }),
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    });
-  
-    if (response.ok) {
-      document.location.replace('/');
-    } else {
-      alert(response.statusText);
-    }
+async function newCaptionHandler(event) {
+  event.preventDefault();
+
+  const caption_text = document.querySelector("#caption-text-area").value;
+  const response = await fetch(`/api/captions`, {
+    method: "POST",
+    body: JSON.stringify({
+      caption_text,
+    }),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
+  if (response.ok) {
+    document.location.replace("/");
+  } else {
+    alert(response.statusText);
   }
-  
-  document.querySelector('.submit-caption').addEventListener('submit', newFormHandler);
-  
+}
+
+document
+  .querySelector(".captionForm")
+  .addEventListener("submit", newCaptionHandler);
