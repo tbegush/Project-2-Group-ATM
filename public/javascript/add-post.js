516ca933962aff05f872bdf10d400b3a49997d68
unknown <alecs2art@gmail.com>
2021-08-15 18:14:38 -0700

    switching branch

--- public/javascript/add-post.js
+++ public/javascript/add-post.js
@@ -20,6 +20,8 @@ async function newFormHandler(event) {
   } else {
     alert(response.statusText);
   }
+
+ 
 }
 
 document.querySelector('.new-post-form').addEventListener('submit', newFormHandler);
